6f28f54429a6925e70ddf65ea1406171237b5934
Henrik Seidel <heseber+github@mailbox.org>
2024-11-18 23:24:54 +0100

    reformatted with black

--- psthumbgen.py
+++ psthumbgen.py
@@ -5,6 +5,7 @@ import argparse
 import errno
 import time
 from PIL import Image
+
 Image.MAX_IMAGE_PIXELS = None
 from multiprocessing import Pool
 from multiprocessing import Value
@@ -13,10 +14,11 @@ import tempfile
 from PIL import ImageOps
 from PIL.ExifTags import TAGS
 
+
 class State(object):
     def __init__(self):
-        self.counter = Value('i', 0)
-        self.start_ticks = Value('d', time.process_time())
+        self.counter = Value("i", 0)
+        self.start_ticks = Value("d", time.process_time())
 
     def increment(self, n=1):
         with self.counter.get_lock():
@@ -56,29 +58,47 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Create thumbnails for Synology Photo Station.")
-    parser.add_argument('-d', "--directory", required=True,
-                        help="Directory to generate thumbnails for. "
-                             "Subdirectories will always be processed.")
-    parser.add_argument("-e", "--eaDir", required=False, default=False, action="store_true",
-                        help="write directly to @eaDir rather than eaDir_tmp")
-    parser.add_argument("-f", "--force", required=False, default=False, action="store_true",
-                        help="force regeneration of all thumbnails.")
+        description="Create thumbnails for Synology Photo Station."
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        required=True,
+        help="Directory to generate thumbnails for. "
+        "Subdirectories will always be processed.",
+    )
+    parser.add_argument(
+        "-e",
+        "--eaDir",
+        required=False,
+        default=False,
+        action="store_true",
+        help="write directly to @eaDir rather than eaDir_tmp",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        required=False,
+        default=False,
+        action="store_true",
+        help="force regeneration of all thumbnails.",
+    )
 
     return parser.parse_args()
 
 
 def find_files(dir):
-    valid_exts = ('jpeg', 'jpg', 'bmp', 'gif', 'png', 'avi', 'mp4', 'mov', 'm4v')
-    valid_exts_re = "|".join(
-        map((lambda ext: ".*\\.{0}$".format(ext)), valid_exts))
+    valid_exts = ("jpeg", "jpg", "bmp", "gif", "png", "avi", "mp4", "mov", "m4v")
+    valid_exts_re = "|".join(map((lambda ext: ".*\\.{0}$".format(ext)), valid_exts))
 
     for root, dirs, files in os.walk(dir):
         for name in files:
-            if re.match(valid_exts_re, name, re.IGNORECASE) \
-                    and not root.endswith('@eaDir') \
-                    and not name.startswith('SYNOPHOTO_THUMB') \
-                    and not name.startswith('.'):
+            if (
+                re.match(valid_exts_re, name, re.IGNORECASE)
+                and not root.endswith("@eaDir")
+                and not name.startswith("SYNOPHOTO_THUMB")
+                and not name.startswith(".")
+            ):
                 yield os.path.join(root, name)
 
 
@@ -87,9 +107,11 @@ def print_progress():
     state.increment(1)
     processed = state.value
     if processed % 10 == 0:
-        print("{0} files processed so far, averaging {1:.2f} files per second."
-              .format(processed, float(processed) /
-                                 (float(time.process_time() - state.start))))
+        print(
+            "{0} files processed so far, averaging {1:.2f} files per second.".format(
+                processed, float(processed) / (float(time.process_time() - state.start))
+            )
+        )
 
 
 def process_file(file_path):
@@ -97,11 +119,11 @@ def process_file(file_path):
     print(file_path)
 
     (dir, filename) = os.path.split(file_path)
-    
-    if (state.eaDir):
-        thumb_dir = os.path.join(dir, '@eaDir', filename)
+
+    if state.eaDir:
+        thumb_dir = os.path.join(dir, "@eaDir", filename)
     else:
-        thumb_dir = os.path.join(dir, 'eaDir_tmp', filename)
+        thumb_dir = os.path.join(dir, "eaDir_tmp", filename)
     ensure_directory_exists(thumb_dir)
 
     create_thumbnails(file_path, thumb_dir)
@@ -119,11 +141,13 @@ def ensure_directory_exists(path):
 
 def create_thumbnails(source_path, dest_dir):
     global state
-    to_generate = (('SYNOPHOTO_THUMB_XL.jpg', 1280),
-                   ('SYNOPHOTO_THUMB_B.jpg', 480),
-                   ('SYNOPHOTO_THUMB_M.jpg', 320),
-                   ('SYNOPHOTO_THUMB_SM.jpg', 240),
-                   ('SYNOPHOTO_THUMB_S.jpg', 90))
+    to_generate = (
+        ("SYNOPHOTO_THUMB_XL.jpg", 1280),
+        ("SYNOPHOTO_THUMB_B.jpg", 480),
+        ("SYNOPHOTO_THUMB_M.jpg", 320),
+        ("SYNOPHOTO_THUMB_SM.jpg", 240),
+        ("SYNOPHOTO_THUMB_S.jpg", 90),
+    )
 
     skip_this = True
     if state.force:
@@ -140,14 +164,28 @@ def create_thumbnails(source_path, dest_dir):
         return
 
     spath_low = source_path.lower()
-    video_ext = ('avi', 'mp4', 'mov', 'm4v') 
+    video_ext = ("avi", "mp4", "mov", "m4v")
     snapshot_file = False
 
     try:
-        if (spath_low.endswith(video_ext)):
+        if spath_low.endswith(video_ext):
             temp_name = next(tempfile._get_candidate_names())
-            snapshot_file=os.path.join(dest_dir, temp_name + ".jpg")
-            subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'error', '-i', source_path, '-ss', '00:00:00.000', '-vframes', '1', snapshot_file])
+            snapshot_file = os.path.join(dest_dir, temp_name + ".jpg")
+            subprocess.call(
+                [
+                    "ffmpeg",
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    source_path,
+                    "-ss",
+                    "00:00:00.000",
+                    "-vframes",
+                    "1",
+                    snapshot_file,
+                ]
+            )
             im = Image.open(snapshot_file)
         else:
             im = Image.open(source_path)
@@ -161,8 +199,6 @@ def create_thumbnails(source_path, dest_dir):
         im = image
     except Exception as e:
         print(e)
-
-
 
     for thumb in to_generate:
         if not state.force:
